b07f6e4cd358f901f9d642c4f3f178742f349539
Shounak Sheshadri Naik <ssnaik@login-02.cm.cluster>
2023-12-16 03:00:02 +0000

    Losses refactor

--- losses.py
+++ losses.py
@@ -10,29 +10,62 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class LossFunctions:
 
     def __init__(self, lambda_content=1., lambda_global=1., lambda_local=1.):
+        """
+        Constructor for the loss function.
+        lambda_content: weight for content loss. If greater than zero content loss is calculated. Else it is not calculated.
+        Content loss has no weight per say, it is binary.
+        lambda_global: propotion of global loss as against the local loss
+        lambda_local: propotion of local loss as against the global loss
+        criterion: criteria of loss calculation
+        
+        """
 
         self.lambda_content = lambda_content
         self.lambda_global = lambda_global
         self.lambda_local = lambda_local
+        ## Mean squared error is chosen as loss evaluation. This done for embeddings
         self.criterion = nn.MSELoss().to(DEVICE)
         self.max_sample = 64 * 64 # TODO
 
     def content_loss(self, stylized_feats, content_feats):
+
+        #Initializing the loss at zero
         loss = torch.tensor(0., device=DEVICE)
+
+        #Calculate the content loss only if content loss weight is greater than zero.
         if self.lambda_content > 0:
+            # Loop over all the layers of the VGG encoder and keep adding the loss
             for i in range(1, 5):
-                loss += self.criterion(mean_variance_norm(stylized_feats[i]),
-                                                       mean_variance_norm(content_feats[i]))
+
+                #Get the normalized features of stylized image and content image respectively from this layer
+                layer_stylized_feats = mean_variance_norm(stylized_feats[i])
+                layer_content_feats = mean_variance_norm(content_feats[i])
+
+                # Calculate loss by calculating the MSE between the 2 embeddings seen above.
+                loss += self.criterion(layer_stylized_feats, layer_content_feats
+                                                       )
         return loss
 
     def style_loss(self, stylized_feats, content_feats, style_feats):
+
+        #Initializing the global loss at zero
         loss_global = torch.tensor(0., device=DEVICE)
+
+        #Calculate the global style loss if the weight is greater than zero
         if self.lambda_global > 0:
+
+            # Loop over all the layers of the VGG encoder and keep adding the global loss
             for i in range(1, 5):
-                s_feats_mean, s_feats_std = calc_mean_std(style_feats[i])
+                # Calculate the mean and standard deviation of the stylized and style distribution from the embedding
+                # of that layer of the VGG encoder
+                style_feats_mean, style_feats_std = calc_mean_std(style_feats[i])
                 stylized_feats_mean, stylized_feats_std = calc_mean_std(stylized_feats[i])
+
+                # Add the absolute difference in mean and standard deviation to the global loss
                 loss_global += self.criterion(
-                    stylized_feats_mean, s_feats_mean) + self.criterion(stylized_feats_std, s_feats_std)
+                    stylized_feats_mean, style_feats_mean) + self.criterion(stylized_feats_std, style_feats_std)
+
+        
         loss_local = torch.tensor(0., device=DEVICE)
         if self.lambda_local > 0:
             for i in range(1, 5):
